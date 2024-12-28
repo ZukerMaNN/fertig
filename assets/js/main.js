9cba6ffcff74e581b0ac73bcf2e45bc77546957b
ZukerMaNN <saxarnuy@gmail.com>
2024-12-28 09:51:15 +0100

    drei  commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $('.header-navbar__btn').ready(function(){
 	$('.header-navbar__btn').click(function(){
 		$(this).toggleClass('open');
-    $('.header-navbar__list').toggleClass('open');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
 	});
 });
